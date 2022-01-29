514e90bab8fc2d8a2d4f94f3e831f04c9d29757e
Collin Newman <45407009+jetskipenguin@users.noreply.github.com>
2022-01-29 12:13:19 -0600

    update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from functions import game
 
 if __name__ == '__main__':
     # ans_choices = ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4']
-    # game.question('Question Text Here', ans_choices, 'Choice 1')
+    # game.question('Question Text Here Test Here Test Here Test Here Test Here', ans_choices, 'Choice 1', weblink='test.com')
     game.start_game()
