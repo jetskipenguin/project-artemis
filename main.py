7813df000ad5e96efaff347b112bf8b00965e060
Collin Newman <45407009+jetskipenguin@users.noreply.github.com>
2022-01-28 20:29:19 -0600

    add question screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from functions import game
 
 if __name__ == '__main__':
-    game.start_game()
+    ans_choices = ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4']
+    game.question('Question Text Here', ans_choices)
